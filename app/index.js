ba4a294fcdad4fd613a73438b3f6a5fda7898ee6
Nifemi <deyeminifemi@gmail.com>
2021-11-29 06:57:31 +0100

    Setup page classes and routing

--- app/index.js
+++ app/index.js
@@ -1,3 +1,76 @@
-import image from './images/5o1dw1.jpg'
+import each from 'lodash/each'
 
-console.log(image)
+import About from 'pages/About'
+import Collections from 'pages/Collections'
+import Detail from 'pages/Detail'
+import Home from 'pages/Home'
+
+class App {
+  constructor() {
+    this.createContent()
+    this.createPages()
+
+    this.addLinkListeners()
+  }
+
+  createContent() {
+    this.content = document.querySelector('.content')
+    this.template = this.content.getAttribute('data-template')
+  }
+
+  createPages() {
+    this.pages = {
+      about: new About(),
+      collections: new Collections(),
+      detail: new Detail(),
+      home: new Home()
+    }
+
+    this.page = this.pages[this.template]
+    this.page.create()
+    this.page.show()
+  }
+
+  async onChange(url) {
+    await this.page.hide()
+
+    const request = await window.fetch(url)
+
+    if (request.status === 200) {
+      const html = await request.text()
+      const div = document.createElement('div')
+
+      div.innerHTML = html
+
+      const divContent = div.querySelector('.content')
+
+      //Get new template and set content div's template id to new template
+      this.template = divContent.getAttribute('data-template')
+
+      this.content.setAttribute('data-template', this.template)
+      this.content.innerHTML = divContent.innerHTML
+
+      this.page = this.pages[this.template]
+      this.page.create()
+      this.page.show()
+    } else {
+      console.log('Error')
+    }
+  }
+
+  addLinkListeners() {
+    const links = document.querySelectorAll('a')
+
+    each(links, link => {
+      link.onclick = event => {
+        event.preventDefault()
+
+        const {href} = link
+
+        this.onChange(href)
+      }
+    })
+  }
+}
+
+new App()
