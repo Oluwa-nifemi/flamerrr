21dfdd2aa54b137c5f95290f77ae7da4501ba7d5
Nifemi <deyeminifemi@gmail.com>
2021-12-21 18:02:26 +0100

    Navigation link animations

--- app/classes/Component.js
+++ app/classes/Component.js
@@ -30,7 +30,13 @@ export default class Component extends EventEmitter {
             if (entry instanceof window.HTMLElement || entry instanceof window.NodeList || Array.isArray(entry)) {
                 this.elements[key] = entry
             } else {
-                this.elements[key] = document.querySelector(entry) || null
+                this.elements[key] = [...document.querySelectorAll(entry)]
+
+                if (this.elements[key].length === 0) {
+                    this.elements[key] = null
+                } else if (this.elements[key].length === 1) {
+                    this.elements[key] = this.elements[key][0]
+                }
             }
         })
     }
--- app/classes/Navigation.js
+++ app/classes/Navigation.js
@@ -1,4 +1,6 @@
 import Component from "./Component";
+import GSAP from "gsap";
+import {COLOR_BRIGHT_GRAY, COLOR_QUARTER_SPANISH_WHITE} from "../utils/colors";
 
 export default class Navigation extends Component {
     constructor({template}) {
@@ -14,6 +16,39 @@ export default class Navigation extends Component {
     }
 
     onChange(template) {
+        console.log(template, this.element, this.elements)
+        if (template === 'about') {
+            GSAP.to(this.element, {
+                color: COLOR_BRIGHT_GRAY,
+                duration: 1.5
+            })
 
+            GSAP.to(this.elements.items[0], {
+                autoAlpha: 1,
+                delay: 0.75,
+                duration: 0.75
+            })
+
+            GSAP.to(this.elements.items[1], {
+                autoAlpha: 0,
+                duration: 0.75
+            })
+        } else {
+            GSAP.to(this.element, {
+                color: COLOR_QUARTER_SPANISH_WHITE,
+                duration: 1.5
+            })
+
+            GSAP.to(this.elements.items[0], {
+                autoAlpha: 0,
+                duration: 0.75
+            })
+
+            GSAP.to(this.elements.items[1], {
+                autoAlpha: 1,
+                delay: 0.75,
+                duration: 0.75
+            })
+        }
     }
 }
